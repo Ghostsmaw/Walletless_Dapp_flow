91d2647265300c1a02dcef17ecc5ea27bb9e5a65
ghostsmaw <ghostsmaw@gmail.com>
2023-04-19 13:29:08 +0100

    integration of a NextJs App and Flow open source api

---
+++ README.md
@@ -0,0 +1,17 @@
+REQUIREMENTS:
+
+1.Go to https://developers.flow.com/tools/flow-cli/install to install the flow cli on your local machine by selecting what platform and following the instructions
+
+2. type ```flow version``` to confirm if it is  installed properly
+
+3. clone this project into your local directory
+
+4.```cd``` into flow_mock and type ```npm install``` to install all dependecies
+
+5.this project uses a local  dev wallet, to startup your emulator, type in the command ```flow start -v```
+
+6. To startup your dev wallet, open another terminal and type ```flow dev-wallet```
+
+7. Now start up the project by typing ```npm run dev``
+
+8. That's all
---
+++ src/pages/index.js
@@ -0,0 +1,95 @@
+import {useState,useEffect} from "react";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin,googleLogout } from '@react-oauth/google';
+import jwt_decode from "jwt-decode";
+import * as fcl from "@onflow/fcl";
+
+
+
+const CLIENT_ID = "894759053387-ol0n2mne9cu5lsmm095h2nbvh521tfqn.apps.googleusercontent.com";
+
+
+
+
+// discovery wallet: http://localhost:8701/fcl/authn
+
+fcl.config({
+  "app.detail.title":"Flow Dapp App",
+  "accessNode.api":"http://localhost:8888",
+  "discovery.wallet":"chrome-extension://hpclkefagolihohboafpheddmmgdffjm/popup.html",
+  "discovery.wallet.method":"EXT/RPC"
+})
+
+
+const decodeToken = (token)=> jwt_decode(token);
+
+export default function Home(){
+  const [user,setUser] = useState({addr:""});
+  const [googleResponse,setGoogleResponse] = useState({});
+  const [profile,setProfile] = useState({});
+
+  const login = (data)=>{
+    console.log(data);
+    setGoogleResponse(data);
+    const payload = decodeToken(data.credential);
+    setProfile(payload);
+  }
+
+  const loginWallet = ()=>{
+    fcl.authenticate();
+  }
+  const logout = ()=>{
+    googleLogout();
+    fcl.unauthenticate();
+    setGoogleResponse({});
+  }
+
+  useEffect(()=>{
+    fcl.currentUser.subscribe(setUser);
+  },[googleResponse,user.addr])
+
+
+  return(
+    <>
+    <GoogleOAuthProvider clientId={`${CLIENT_ID}`}>
+      <div className="flex justify-center items-center w-full h-screen bg-green-400">
+        {
+          Object.keys(googleResponse).length < 1 &&
+          <GoogleLogin onSuccess={login} onError={()=>alert("Error occured while signing in")}/>
+        }
+        {
+           Object.keys(googleResponse).length > 1 &&
+           (
+            user.addr ?
+            <div className="flex flex-col w-full items-center justify-center">
+              <div className="w-full mb-8 font-bold text-white text-3xl text-center">
+                Welcome, {profile.email}
+              </div>
+            <span className="text-lg font-semibold">
+           {user?.addr}
+           <button className="bg-blue-200 shadow-lg h-12 w-30 text-lg text-[#3f3f3f] font-bold p-4 rounded-sm flex items-center hover:bg-blue-300" onClick={logout}>
+             Logout
+           </button>
+         </span>
+            </div>:
+        <div className="flex flex-col w-full items-center justify-center">
+          <div className="w-full mb-8 font-bold text-white text-3xl text-center">
+             Welcome, {profile.email}
+          </div>
+            <div className="text-lg font-semibold flex gap-3">
+            <button className="bg-blue-200 shadow-lg h-12 w-30 text-lg text-black font-bold p-4 rounded-sm flex items-center hover:bg-blue-300" onClick={loginWallet}>
+              Connect to your wallet to login
+            </button>
+
+            <button className="bg-blue-200 shadow-lg h-12 w-30 text-lg text-black font-bold p-4 rounded-sm flex items-center hover:bg-blue-300"  onClick={logout}>
+              Logout
+            </button>
+          </div>
+        </div>
+           )
+        }
+      </div>
+    </GoogleOAuthProvider>
+    </>
+  )
+}
